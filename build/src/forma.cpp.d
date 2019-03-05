d299047601c320c4022f6393ad63b28659deebd5
Arden Rasmussen <ardenisthebest@gmail.com>
2019-03-04 23:14:44 -0800

    Removed build files

--- build/src/forma.cpp.d
+++
@@ -1,2 +0,0 @@
-/home/arden/Programming/cpp/forma/build/src/forma.cpp.o: src/forma.cpp \
-  src/forma.hpp
--- build/src/main.cpp.d
+++
@@ -1,2 +0,0 @@
-/home/arden/Programming/cpp/forma/build/src/main.cpp.o: src/main.cpp \
-  src/logger.hpp
