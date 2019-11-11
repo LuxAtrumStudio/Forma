9c04da0f218fcb148de2ec1477daef3b0781edcc
Arden Rasmussen <ardenisthebest@gmail.com>
2019-11-10 20:50:27 -0800

    Initial commit

---
+++ include/forma/forma.hpp
@@ -0,0 +1,9 @@
+#ifndef FORMA_HPP_
+#define FORMA_HPP_
+
+/**
+ * @brief Core graphics engine namespace
+ */
+namespace forma {}  // namespace forma
+
+#endif  // FORMA_HPP_
--- src/window.hpp
+++
@@ -1,1 +0,0 @@
-#include "window/window_class.hpp"
